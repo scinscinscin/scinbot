8b24d020645880e3504c1ebfda4c3c93692910d4
scinorandex <scinorandex@gmail.com>
2021-01-28 16:49:25 +0800

    add commit info to test command

--- commands/test.js
+++ commands/test.js
@@ -1,9 +1,12 @@
+const fs = require('fs');
+const commit = fs.readFileSync(`gitCommit`, {encoding:'utf8', flag:'r'});
 const { colors } = require('../config/main.json');
 const green = colors.green;
 
 function test(channelID, authorID, author, args){
     message =  "**";
     message += "Info: \n";
+    message += `Commit Hash: ${commit}`;
     message += `Author: ${author} \n`;
     message += `Author ID: ${authorID} \n`;
     message += `Channel ID: ${channelID} \n`;
--- docker-build.sh
+++ docker-build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+git rev-parse HEAD > gitCommit
 docker build -t docker.scinorandex.xyz/scinorandex/scinbot .
 docker push docker.scinorandex.xyz/scinorandex/scinbot
